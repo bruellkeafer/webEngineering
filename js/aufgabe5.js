934b5312fae9fba5da65076193152cc30da6b6cc
Tim Martins <48648945+bruellkeafer@users.noreply.github.com>
2021-11-29 17:42:42 +0100

    weiterarbeiten

--- js/aufgabe5.js
+++ js/aufgabe5.js
@@ -29,22 +29,25 @@ let heater = {
     min: 0,
     max: 0,
     step: 0,
-    read: function(){
+    read(){
         this.min = parseInt(document.getElementById("min").value)
         this.max =  parseInt(document.getElementById("max").value)
         this.step = parseInt(document.getElementById("step").value)
         document.getElementById("temp").innerHTML = this.temp
         printStatus()
+        return this
     },
-    up: function(){
+    up(){
         this.temp = this.temp + this.step
         if(this.temp > this.max) this.temp = this.max;
         document.getElementById("temp").innerHTML = this.temp
+        return this
     },
-    down: function(){
+    down(){
         this.temp -= this.step
         if(this.temp < this.min) this.temp = this.min;
         document.getElementById("temp").innerHTML = this.temp
+        return this
     }
 }
 
@@ -73,11 +76,51 @@ let user = {
     console.log("Hi, my name is " + this.name);
     }
     };
-    let admin = {
+let admin = {
     name: "Bob",
     }
+
     admin.sayHi = user.sayHi;
     user.sayHi();
     admin.sayHi();
-    let greet = user.sayHi;
+
+    let greet = function(){user.sayHi()};
     greet();
+
+//4
+greet = user.sayHi.bind(user)
+greet()
+
+//5
+heater.min = 0;
+heater.max = 100;
+heater.step = 33;
+
+heater.up().up().up();
+
+//6
+let person = {
+    name : "",
+    female : false,
+    _age : 1,
+    get getAge(){
+        if(typeof(this._age) === "number" && this._age == !null ) return this._age;
+
+    },
+    set setAge(a){
+        if(typeof(a) === "number" && a != null) {
+            this._age = a;
+            console.log(a)
+        }
+        
+    },
+    sayHi(){
+        console.log("Hey, ich bin " + this.name + ", " + (this.female ? "weiblich" : "männlich") + ", und " + this._age + " Jahre jung. Schön dich kennen zu lernen.")
+    }
+};
+
+person.setAge = 42
+person.name = "Bob"
+person.female = true
+console.log(person.getAge);
+person.sayHi()
