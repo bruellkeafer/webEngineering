5bf6e6358f14b9924b79f4bcc58634ce2d9882be
Tim Martins <48648945+bruellkeafer@users.noreply.github.com>
2021-11-29 15:32:37 +0100

    UBG_5 weiter bearbeitet hab ein Bild ins repos aufgenommen, da der Link nicht mehr funktioniert hat

--- js/aufgabe5.js
+++ js/aufgabe5.js
@@ -1,5 +1,7 @@
 "use strict"; 
 
+
+//1
 function multiplyNumeric(obj){
     for (let property in obj){
         if(typeof(obj[property]) === "number"){
@@ -18,3 +20,64 @@ let menu = {
 multiplyNumeric(menu)
 
 console.log(menu)
+
+
+//2
+
+let heater = {
+    temp: 0,
+    min: 0,
+    max: 0,
+    step: 0,
+    read: function(){
+        this.min = parseInt(document.getElementById("min").value)
+        this.max =  parseInt(document.getElementById("max").value)
+        this.step = parseInt(document.getElementById("step").value)
+        document.getElementById("temp").innerHTML = this.temp
+        printStatus()
+    },
+    up: function(){
+        this.temp = this.temp + this.step
+        if(this.temp > this.max) this.temp = this.max;
+        document.getElementById("temp").innerHTML = this.temp
+    },
+    down: function(){
+        this.temp -= this.step
+        if(this.temp < this.min) this.temp = this.min;
+        document.getElementById("temp").innerHTML = this.temp
+    }
+}
+
+
+var readButton = document.getElementById("read")
+var upButton = document.getElementById("up")
+var downButton = document.getElementById("down")
+readButton.onclick = function(){heater.read()}
+upButton.onclick = function(){heater.up()}
+downButton.onclick = function(){heater.down()}
+
+var printStatus = function(){
+console.log("#")
+console.log(heater.temp)
+console.log(heater.max)
+console.log(heater.min)
+console.log(heater.step)
+}
+
+
+//3
+let user = {
+    name: "Smith",
+    age: 30,
+    sayHi() {
+    console.log("Hi, my name is " + this.name);
+    }
+    };
+    let admin = {
+    name: "Bob",
+    }
+    admin.sayHi = user.sayHi;
+    user.sayHi();
+    admin.sayHi();
+    let greet = user.sayHi;
+    greet();
---
+++ js/view.js
@@ -0,0 +1,5 @@
+//var standardTemplate = document.getElementById("Blog_Overview")
+//standardTemplate.hidden = true
+//var inputTemplate = document.getElementById("UBG_5")
+//inputTemplate.hidden = false
+
